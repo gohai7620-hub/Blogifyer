ecfed9f4b15f2c7fb7f44c3bbbff7f4cdc9ef09a
MR_DIV <vshntvelip@gmail.com>
2026-05-16 17:16:42 +0530

    Fix: Correct case-sensitive require path for Blog model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 
 const UserRoute = require("./routes/User");
 const UserBlogsRoute = require("./routes/Blog");
-const Blog = require("./models/blog");
+const Blog = require("./models/Blog");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 dotenv.config();
