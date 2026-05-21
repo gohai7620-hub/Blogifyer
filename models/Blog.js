eaf9a3d89dd98c8f623aa9000a66ce677aa58512
MR_DIV <vshntvelip@gmail.com>
2026-05-21 18:40:37 +0530

    Update Blog.js

--- models/Blog.js
+++ models/Blog.js
@@ -2,20 +2,13 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const BlogSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    body: {
-        type: String,
-        required: true,
-    },
-    coverImageURL: {
-        type: String,
-    },
-    createdBy: {
-        type: Schema.Types.ObjectId,
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    coverImageURL: { type: String, default: null },
+    createdBy: { 
+        type: Schema.Types.ObjectId, 
         ref: "user",
+        required: true 
     }
 }, { timestamps: true });
 
