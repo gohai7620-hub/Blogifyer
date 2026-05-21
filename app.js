39fde7f8e77ec9bf7e9c2a3ce6aec4539effed74
MR_DIV <vshntvelip@gmail.com>
2026-05-21 20:39:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { checkForAuthenticationCookie } = require("./middlewares/authentication")
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Load dotenv in development only
+// ====================== ENVIRONMENT VARIABLES ======================
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
@@ -20,9 +20,8 @@ if (process.env.NODE_ENV !== "production") {
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-    console.error("❌ MONGODB_URI is missing in Environment Variables!");
+    console.error("❌ MONGODB_URI is missing!");
 } else {
-    console.log("🔗 Trying to connect to MongoDB Atlas...");
     mongoose.connect(MONGODB_URI, {
         serverSelectionTimeoutMS: 30000,
         socketTimeoutMS: 60000,
