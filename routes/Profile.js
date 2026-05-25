cb2ceaa6d229fd167ffadbb2cd4d364477adb480
webdeveloper10908-bit <webdeveloper10908@gmail.com>
2026-05-25 10:07:05 +0530

    Update Profile.js

--- routes/Profile.js
+++ routes/Profile.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const Blog = require("../models/Blog");
-
 const { restrictToLoggedInUserOnly } = require("../middlewares/authentication");
 
 router.use(restrictToLoggedInUserOnly);
 
 router.get("/", async (req, res) => {
     try {
-        const userBlogs = await Blog.find({ createdBy: req.user._id })
+        const { search } = req.query;
+        const filter = { createdBy: req.user._id };
+
+        if (search) {
+            filter.$or = [
+                { title: { $regex: search, $options: "i" } },
+                { body: { $regex: search, $options: "i" } }
+            ];
+        }
+
+        const userBlogs = await Blog.find(filter)
             .sort({ createdAt: -1 })
             .lean();
 
         res.render("profile", {
             user: req.user,
-            blogs: userBlogs
+            blogs: userBlogs,
+            search: search || ""
         });
     } catch (error) {
         console.error("Profile Error:", error);
